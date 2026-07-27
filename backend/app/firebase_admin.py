28edca245235c1fc5b1e10ba4e37d46ee1a6a912
mariam <beruashvilimariam10@gmail.com>
2026-07-27 13:52:36 +0200

    c

--- backend/app/firebase_admin.py
+++ backend/app/firebase_admin.py
@@ -1,14 +1,28 @@
 import os
 import json
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, auth
+from dotenv import load_dotenv
+
+# Solo carga .env en desarrollo local
+load_dotenv()
 
 if not firebase_admin._apps:
     firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
     if firebase_json:
+        # Render: lee el JSON desde la variable de entorno
         cred = credentials.Certificate(json.loads(firebase_json))
     else:
-        cred = credentials.Certificate("backend/secrets/serviceAccountKey.json")
+        # Local: lee el archivo
+        cred = credentials.Certificate("secrets/serviceAccountKey.json")
 
     firebase_admin.initialize_app(cred)
+
+
+def verify_firebase_token(token: str):
+    try:
+        decoded_token = auth.verify_id_token(token)
+        return decoded_token
+    except Exception:
+        return None
