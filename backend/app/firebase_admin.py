f1172270bc2dd3edee1ce9ff9e68ae72af261822
mariam <beruashvilimariam10@gmail.com>
2026-07-27 14:10:14 +0200

    b

--- backend/app/firebase_admin.py
+++ backend/app/firebase_admin.py
@@ -1,16 +1,17 @@
 import os
 import json
-from firebase_admin import credentials, initialize_app
+import firebase_admin
+from firebase_admin import credentials
 
-# 1. Intentar leer las credenciales desde una variable de entorno (Render)
+# 1. Intentar leer desde la variable de entorno de Render
 firebase_json_env = os.getenv("FIREBASE_CREDENTIALS_JSON")
 
 if firebase_json_env:
-    # Cargar el diccionario directamente desde el string JSON
+    # Cargar las credenciales directamente desde el texto JSON
     cred_dict = json.loads(firebase_json_env)
     cred = credentials.Certificate(cred_dict)
 else:
-    # 2. Si no hay variable, usar el archivo local (Desarrollo local)
+    # Respaldo para cuando desarrollas de manera local en tu PC
     cred = credentials.Certificate("secrets/serviceAccountKey.json")
 
-initialize_app(cred)
+firebase_admin.initialize_app(cred)
